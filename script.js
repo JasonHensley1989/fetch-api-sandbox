c1d40b4afd6ed204da402ae76bb89df49e805d54
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-09 12:07:31 -0600

    get testing

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ console.log("Connected");
 document.getElementById('getText').addEventListener('click', getText);
 
 function getText() {
-    console.log(123);
+    fetch('sample.txt')
+        .then(res => res.text())
+        .then(data => console.log(data))
+
 }
