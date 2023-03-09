470f529f93fab9743dc869a80cb01f032a2e5f7b
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-09 12:53:26 -0600

    3rd party json api holder, form following

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// This fetches a text file locally
+
 const getText = () => {
     fetch('sample.txt')
         .then(res => res.text())
@@ -6,6 +8,8 @@ const getText = () => {
         })
         .catch(err => console.log("Error"))
 }
+
+// Fetches a local JSON file and converts to JS
 
 const getUsers = () => {
     fetch('users.json')
@@ -24,8 +28,26 @@ const getUsers = () => {
             document.getElementById('output').innerHTML = output;
         })
 }
+// Fetches a remote API and converts to JS
 
+const getPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => {
+            let output = "<h2>Posts</h2>";
+            data.forEach(post => {
+                output += `
+                    <div>
+                        <h3>${post.title}</h3>
+                        <p>${post.body}</p>
+                    </div>
+                `;
+            })
+            document.getElementById('output').innerHTML = output;
+        })
+}
 // Event Listeners
 
 document.getElementById('getUsers').addEventListener('click', getUsers);
 document.getElementById('getText').addEventListener('click', getText);
+document.getElementById('getPosts').addEventListener('click', getPosts);
